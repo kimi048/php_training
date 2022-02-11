119c38ca73d00c8a6bfd24202a672904b15c3774
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-02-11 22:07:47 +0900

    PHP and HTML

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+$goofie="goofie"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+
+
+
+<?php
 
 // echo "kimikimi";
 $cars = array("Nissan", "Toyota", "VW");
@@ -6,4 +22,13 @@ $colors = array("red", "green", "blue", "yellow");
 foreach($cars as $key =>$value ){
   print($key." : ".$value."<br>");
 }
+echo count($cars)."<br>";
+echo "Hello ".$goofie;
 ?>
+</body>
+<script>
+const newUser='<?php echo $goofie?>';
+console.log(newUser);
+</script>
+</html>
+
