c69786e806af66d428986dc8c8101023bf1e0284
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-02-11 21:23:41 +0900

    array

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?
+<?php
 
-echo "kimikimi"
-
+// echo "kimikimi";
+$cars = array("Nissan", "Toyota", "VW");
+$colors = array("red", "green", "blue", "yellow");
+foreach($cars as $key =>$value ){
+  print($key." : ".$value."<br>");
+}
 ?>
